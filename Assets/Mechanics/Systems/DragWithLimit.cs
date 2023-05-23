14e0bea40c3cae418f0bed12b78e310618d6637f
Căbău Natan <cabau.natan.dev@gmail.com>
2023-05-23 18:35:07 +0300

    improve

---
+++ Assets/Mechanics/Systems/DragWithLimit.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class DragWithLimit : MonoBehaviour
+{
+    [SerializeField]
+    private HingeJoint hingeJoint;
+
+    [SerializeField]
+    private float maxDistance = 2f; // Valoarea maximă pentru distanța permisă între obiectul de tragere și ragdoll
+
+    private Vector3 originalPosition;
+    private float originalDistance;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        originalDistance = Vector3.Distance(transform.position, hingeJoint.connectedBody.transform.position);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, originalDistance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        float distance = Vector3.Distance(objPosition, hingeJoint.connectedBody.transform.position);
+
+        if (distance <= maxDistance)
+        {
+            hingeJoint.connectedAnchor = objPosition;
+        }
+        else
+        {
+            Vector3 direction = (objPosition - hingeJoint.connectedBody.transform.position).normalized;
+            Vector3 limitedPosition = hingeJoint.connectedBody.transform.position + (direction * maxDistance);
+            hingeJoint.connectedAnchor = limitedPosition;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        hingeJoint.connectedAnchor = originalPosition;
+    }
+}
+
---
+++ Assets/Mechanics/Systems/LimitArmStretch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LimitArmStretch : MonoBehaviour
+{
+    private ConfigurableJoint joint;
+
+    [SerializeField]
+    private float maxXLimit = 45f; // Valoarea limită maximă pe axa X
+
+    [SerializeField]
+    private float maxYLimit = 45f; // Valoarea limită maximă pe axa Y
+
+    private void Start()
+    {
+        joint = GetComponent<ConfigurableJoint>();
+
+        // Setează mișcarea limitată pe axele X și Y
+        joint.angularXMotion = ConfigurableJointMotion.Limited;
+        joint.angularYMotion = ConfigurableJointMotion.Limited;
+
+        // Setează limitele de rotație pentru axa X
+        SoftJointLimit limitX = new SoftJointLimit();
+        limitX.limit = maxXLimit;
+        joint.lowAngularXLimit = limitX;
+        joint.highAngularXLimit = limitX;
+
+        // Setează limitele de rotație pentru axa Y
+        SoftJointLimit limitY = new SoftJointLimit();
+        limitY.limit = maxYLimit;
+        joint.angularYLimit = limitY;
+    }
+}
